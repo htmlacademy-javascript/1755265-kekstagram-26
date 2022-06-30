4d4a3ae0adf74f06aa7f72fb50cbdd5a608e0b18
comeunne <itspawses@gmail.com>
2022-06-30 23:54:05 +0900

    добавляет, реализует main.js и следует критериям

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 function checkStringLength(string, size) {
   return string.length > size;
-};
+}
 
 function getRandomNumber(min, max) {
   if (min < 0 || max < 0) {
     throw new RangeError(`Only non-negative range allowed([${min}, ${max}])`);
   }
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 checkStringLength('some text',7);
 getRandomNumber(5,10);
+
