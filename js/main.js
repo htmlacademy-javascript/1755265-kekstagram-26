3ef9eee1d5cb673bcf94e4240d025dbe5c2cdc48
comeunne <itspawses@gmail.com>
2022-06-30 23:47:58 +0900

    добавляет, реализует main.js и следует критериям

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-const checkStringLength =(string,size ) =>
-  string.length > size;
+const checkStringLength = (string,size) => {
+  return string.length > size;
+}
 
-const getRandomNumber = function(min,max) {
-  if (min < 0  || max < 0 ) {
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0) {
     throw new RangeError(`Only non-negative range allowed([${min}, ${max}])`);
   }
-  return Math.floor(Math.random() * (max-min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
 checkStringLength('some text',7);
